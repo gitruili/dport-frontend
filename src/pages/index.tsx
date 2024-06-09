78a6df356203cd0115146c523e71a431ac4ca00a
gitruili <woshilirui@hotmail.com>
2024-06-09 22:19:07 +0800

    change home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { HomeView } from "../views";
+import Leaderboard from "views/Leaderboard";
 
 const Home: NextPage = (props) => {
   return (
@@ -12,7 +13,7 @@ const Home: NextPage = (props) => {
           content="DPORT"
         />
       </Head>
-      <HomeView />
+      <Leaderboard />
     </div>
   );
 };
